5fb59a27bc9d1af517d486fe7afd8cb3b7f7c282
ArockiaSybilPerez <109013417+ArockiaSybilPerez@users.noreply.github.com>
2022-07-24 12:34:43 +0530

    org.system
    org.system

---
+++ Desktop.java
@@ -0,0 +1,28 @@
+package org.system;
+
+//Package   :org.system
+//Class        :Computer
+//Methods   :computerModel()
+
+//Class        :Desktop
+//Methods   :desktopSize()
+
+//Description:
+//create above 2 class and call all your class methods into the Desktop using single inheritance.
+public class Desktop extends Computer {
+	
+	public void DesktopSize(){
+		System.out.println("Desktop Size is 7");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Desktop obj1 = new Desktop();
+		obj1.DesktopSize();
+		
+		Desktop obj = new Desktop();
+		obj.computerModel();
+		
+	}
+}
