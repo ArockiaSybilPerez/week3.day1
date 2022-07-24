edd0689557897a7e9e4206af66776fd13824abcf
ArockiaSybilPerez <109013417+ArockiaSybilPerez@users.noreply.github.com>
2022-07-24 12:14:08 +0530

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,13 @@
+package org.department;
+
+import org.college.College;
+
+//     Package   :org.department
+//     Class     :Department
+//     Methods   :deptName()
+public class Department extends College{
+	public void deptName() {
+		System.out.println("Dept Name is ETCE");	
+	}
+}
+
