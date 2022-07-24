9a113962df320d92933f5f6a50fce25ab84ece58
ArockiaSybilPerez <109013417+ArockiaSybilPerez@users.noreply.github.com>
2022-07-24 12:33:21 +0530

    assignment5.abstraction
    assignment5.abstraction

---
+++ Language.java
@@ -0,0 +1,14 @@
+package assignment5.abstraction;
+
+//Interface :Language
+//Methods   :Java()
+//Interface :TestTool
+//Methods   :Selenium()
+//AbstractClass :MultipleLangauge
+//Methods   :python() and un implemented method as ruby()
+//Execution class: Automation 
+//mplement all the methods of interface and abstract class in Automation class
+
+public interface Language {
+	public void Java();
+}
